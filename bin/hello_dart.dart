4bba9d378b5a3971f87775438f95e3fed46b47c1
abdulrasheed1729 <fawomath@gmail.com>
2021-10-15 02:29:30 +0100

    just a little change

--- bin/hello_dart.dart
+++ bin/hello_dart.dart
@@ -1,3 +1,14 @@
+import 'dart:math';
+
 void main() {
   print('Hello world!');
+  print(22/7);
+  print(sin(pi * 1/4));
+  print(1/sqrt(2) == sin(pi* 1/ 4));
+  print(10.isOdd);
+
+  dynamic myNumber ;
+  myNumber = 9.33;
+  myNumber = 23;
+  print(myNumber);
 }
